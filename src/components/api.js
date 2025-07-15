c0d0b1fc349f85e609132545ac47390d79191a0f
Sardor Bees <sardor@example.com>
2025-07-15 18:57:48 +0500

    Initial commit

--- src/components/api.js
+++ src/components/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://backend-dedd.onrender.com",
+  baseURL: "https://backend-dedd.onrender.com/",
 });
 
 API.interceptors.request.use((config) => {
